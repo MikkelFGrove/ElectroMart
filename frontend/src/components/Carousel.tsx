59072a9290ab7b19e7b7c03e816f3c750fa507b3
Madsando <madsjep23@gmail.com>
2026-03-19 12:54:49 +0100

    E5: Retry  Product Recommendations if Error occurs

--- frontend/src/components/Carousel.tsx
+++ frontend/src/components/Carousel.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import axiosRetry from 'axios-retry';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import { Navigation } from 'swiper/modules';
@@ -11,6 +12,16 @@ import 'swiper/css/navigation'; // navigation module
 const Carousel = () => {
   const [products, setProducts] = useState<any[]>([]);
 
+  axiosRetry(axios,
+    {
+      retries : 5,
+      retryCondition: (error) => {
+            console.log('Error fetching products:', error);
+            return error.response?.status === 500;
+        },
+    }
+  )
+
   useEffect(() => {
     const fetchProducts = async () => {
       try {
@@ -18,7 +29,7 @@ const Carousel = () => {
         setProducts(response.data);
       } catch (error) {
         console.error('Error fetching products:', error);
-        setProducts([]);
+        setProducts([{"title":"Smartphone Galaxy G20","imageUrl":"https://cdn.lomax.dk/images/t_item_max/f_auto/v1630998173/produkter/70122740_1/samsung-galaxy-z-fold3-5g-256gb-smartphone-sort-1.jpg","basePrice":1099,"taxRate":1.25,"discountRate":0.25,"productType":"Phone"}]);
       }
     };
 
